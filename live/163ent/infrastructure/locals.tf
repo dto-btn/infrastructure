ea71d405df58de0f5efb6131b649fffebedd4bee
Andrew Ruan <andrew.ruan@ssc-spc.gc.ca>
2025-09-23 14:42:18 -0400

    separating the resources

--- live/163ent/infrastructure/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-    subscription_name = "ScSc-PBMM CIO ECT"
-    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
-    name_prefix = "ScSc-CIO_ECT_Infra"
-    tenant_id = "d05bc194-94bf-4ad6-ae2e-1db0f2e38f5e" #where do we want this?  depends on how we plan on settign this up.  hardcoding here for now.
-}
