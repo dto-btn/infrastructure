65c3db41ec15ac43543623ec7a5d9043a21d78df
Andrew Ruan <andrew.ruan@ssc-spc.gc.ca>
2025-09-24 12:48:06 -0400

    acr names restrictions.  common prefix wont work

--- modules/global/main.tf
+++ modules/global/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_key_vault" "infrakv" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.name_prefix}-acr"
+  name                = "${var.acr_name}"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
   sku                 = "Basic"
--- modules/global/variables.tf
+++ modules/global/variables.tf
@@ -6,6 +6,10 @@ variable "name_prefix" {
     type = string
 }
 
+variable "acr_name" {
+    type = string
+}
+
 variable "tenant_id" {
     type = string
 }
