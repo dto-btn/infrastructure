5a11034eab528979f91b02732b3f46366d7dc685
Andrew Ruan <andrew.ruan@ssc-spc.gc.ca>
2025-09-24 16:15:02 -0400

    output for log analytics name

--- modules/global/output.tf
+++ modules/global/output.tf
@@ -5,3 +5,7 @@ output "key_vault_name" {
 output "acr_name" {
     value = azurerm_container_registry.acr.name
 }
+
+output "log_analytics_workspace_name" {
+    value = azurerm_log_analytics_workspace.logAnalytics.name
+}
