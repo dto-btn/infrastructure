6ddf1cb36e806f54fea1d1da36983bee113c6965
bigyam <aruan.careers@gmail.com>
2025-12-03 15:27:31 -0500

    cleanup

--- modules/ssca-cluster/cae.tf
+++ modules/ssca-cluster/cae.tf
@@ -1,3 +1,17 @@
+data "azurerm_subscription" "current" {
+  subscription_id = var.subscription_id
+}
+
+data "azuread_application" "container_app_app_reg" {
+  display_name = var.app_registation_name
+}
+
+resource "azuread_application_password" "containerAppSecret" {
+  application_id = data.azuread_application.container_app_app_reg.id
+  end_date = timeadd(timestamp(), "4320h") #180 days
+  display_name = "${var.container_app.name}-containerapp-secret"
+}
+
 resource "azurerm_container_app_environment" "containerAppEnv" {
   name                       = var.container_app_environment_name
   location                   = azurerm_resource_group.rg.location
@@ -20,6 +34,11 @@ resource "azurerm_container_app" "containerApp" {
   identity{
     type = "UserAssigned"
     identity_ids = [azurerm_user_assigned_identity.mcpImageIdentity.id] 
+  }
+
+  secret {
+    name = "microsoft-provider-authentication-secret"
+    value = azuread_application_password.containerAppSecret.value
   }
 
   template {
@@ -52,15 +71,11 @@ resource "azurerm_container_app" "containerApp" {
   }
 }
 
-data "azurerm_subscription" "current" {
-  subscription_id = var.subscription_id
-}
-
-data "azuread_application" "container_app_app_reg" {
-  display_name = var.app_registation_name
-}
-
 resource "azapi_resource_action" "container_app_auth_settings" {
+  depends_on = [
+    azurerm_container_app.containerApp, azuread_application_password.containerAppSecret
+  ]
+
   type      = "Microsoft.App/containerApps/authConfigs@2025-02-02-preview"
   resource_id = "${azurerm_container_app.containerApp.id}/authConfigs/current"
 
@@ -104,43 +119,6 @@ resource "azapi_resource_action" "container_app_auth_settings" {
               nonce: {}
           },
           encryptionSettings: {}
-    }
-  }
-}
-
-resource "azurerm_container_app" "testcontainerApp" {
-  name                         = "testcaontinerapp"
-  container_app_environment_id = azurerm_container_app_environment.containerAppEnv.id
-  resource_group_name          = azurerm_resource_group.rg.name
-  revision_mode                = var.container_app.revision_mode
-
-  registry{
-    identity = azurerm_user_assigned_identity.mcpImageIdentity.id
-    server = data.azurerm_container_registry.acr.login_server
-  }
-
-  identity{
-    type = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.mcpImageIdentity.id] 
-  }
-
-  template {
-    container {
-      image = "${data.azurerm_container_registry.acr.login_server}/${var.acr.image.repo_name}:${var.acr.image.tag}"
-      name  = "testcaontinerapp-container"
-      cpu    = 2
-      memory = "4Gi"
-    }
-  }
-
-  ingress {
-    external_enabled = true
-    client_certificate_mode = "ignore"
-    target_port = 8000
-    transport = "auto"
-    traffic_weight {
-      latest_revision = true
-      percentage = 100
     }
   }
 }
