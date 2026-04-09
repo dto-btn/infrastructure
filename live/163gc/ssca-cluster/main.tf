7a294bf14a0e523e16168bc486f1bafe9483189f
jeneerthan <jeneerthan.pageerathan@ssc-spc.gc.ca>
2026-04-09 13:22:01 -0400

    Added litellm proxy and set min replica to 1 for all mcp container apps

--- live/163gc/ssca-cluster/main.tf
+++ live/163gc/ssca-cluster/main.tf
@@ -1,161 +1,3 @@
-module "dev" {
-  source = "../../../modules/ssca-cluster"
-
-  resource_group = "ScSc-CIO-ECT_SSCA_Cluster_Dev_RG"
-  create_resource_group = true
-  create_container_app_env = true
-  location = "canadacentral"
-  acr = {
-    name = "ectacr"
-    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
-    image = {
-      repo_name = "ssca-mcp-server"
-      tag = "1.0.0"
-    }
-  }
-  log_analytics = {
-    name = "ScSc-CIO-ECT-Infra-analytics"
-    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
-  }
-  container_app_environment_name = "ssca-cae"
-  container_app = {
-    name = "ssca-mcp-server"
-    revision_mode = "Single"
-    min_replicas  = 1
-  }
-  subscription_id = "f5fb90f1-6d1e-4a21-8935-6968d811afd8"
-  app_registration_name = "SSC-Assistant-Dev"
-
-  env_vars = var.ssca_mcp_env_vars
-  secrets  = var.ssca_mcp_secrets
-}
-
-module "geds" {
-  source = "../../../modules/ssca-cluster"
-
-  resource_group = "ScSc-CIO-ECT_SSCA_Cluster_Dev_RG"
-  create_resource_group = false
-  create_container_app_env = false
-  location = "canadacentral"
-  acr = {
-    name = "ectacr"
-    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
-    image = {
-      repo_name = "geds-mcp"
-      tag = "1.0.0"
-    }
-  }
-  log_analytics = {
-    name = "ScSc-CIO-ECT-Infra-analytics"
-    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
-  }
-  container_app_environment_name = "ssca-cae"
-  container_app = {
-    name = "geds-mcp"
-    revision_mode = "Single"
-  }
-  subscription_id = "f5fb90f1-6d1e-4a21-8935-6968d811afd8"
-  app_registration_name = "SSC-Assistant-Dev"
-
-  env_vars = var.geds_mcp_env_vars
-  secrets  = var.geds_mcp_secrets
-}
-
-module "pmcoe" {
-  source = "../../../modules/ssca-cluster"
-
-  resource_group = "ScSc-CIO-ECT_SSCA_Cluster_Dev_RG"
-  create_resource_group = false
-  create_container_app_env = false
-  location = "canadacentral"
-  acr = {
-    name = "ectacr"
-    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
-    image = {
-      repo_name = "azure-search-mcp"
-      tag = "1.0.0"
-    }
-  }
-  log_analytics = {
-    name = "ScSc-CIO-ECT-Infra-analytics"
-    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
-  }
-  container_app_environment_name = "ssca-cae"
-  container_app = {
-    name = "pmcoe-mcp"
-    revision_mode = "Single"
-    min_replicas  = 1
-  }
-  subscription_id = "f5fb90f1-6d1e-4a21-8935-6968d811afd8"
-  app_registration_name = "SSC-Assistant-Dev"
-
-  env_vars = var.pmcoe_mcp_env_vars
-  secrets  = var.pmcoe_mcp_secrets
-}
-
-module "myssc" {
-  source = "../../../modules/ssca-cluster"
-
-  resource_group = "ScSc-CIO-ECT_SSCA_Cluster_Dev_RG"
-  create_resource_group = false
-  create_container_app_env = false
-  location = "canadacentral"
-  acr = {
-    name = "ectacr"
-    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
-    image = {
-      repo_name = "azure-search-mcp"
-      tag = "1.0.0"
-    }
-  }
-  log_analytics = {
-    name = "ScSc-CIO-ECT-Infra-analytics"
-    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
-  }
-  container_app_environment_name = "ssca-cae"
-  container_app = {
-    name = "myssc-mcp"
-    revision_mode = "Single"
-  }
-  subscription_id = "f5fb90f1-6d1e-4a21-8935-6968d811afd8"
-  app_registration_name = "SSC-Assistant-Dev"
-
-  env_vars = var.myssc_mcp_env_vars
-  secrets  = var.myssc_mcp_secrets
-}
-
-module "bits" {
-  source = "../../../modules/ssca-cluster"
-
-  resource_group = "ScSc-CIO-ECT_SSCA_Cluster_Dev_RG"
-  create_resource_group = false
-  create_container_app_env = false
-  location = "canadacentral"
-  acr = {
-    name = "ectacr"
-    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
-    image = {
-      repo_name = "bits-mcp"
-      tag = "1.0.0"
-    }
-  }
-  log_analytics = {
-    name = "ScSc-CIO-ECT-Infra-analytics"
-    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
-  }
-  container_app_environment_name = "ssca-cae"
-  container_app = {
-    name          = "bits-mcp"
-    revision_mode = "Single"
-    min_replicas  = 1
-  }
-  subscription_id = "f5fb90f1-6d1e-4a21-8935-6968d811afd8"
-  app_registration_name = "SSC-Assistant-Dev"
-
-  env_vars = var.bits_mcp_env_vars
-  secrets  = var.bits_mcp_secrets
-}
-
 resource "random_password" "litellm_db_password" {
   length           = 24
   special          = true
@@ -206,7 +48,172 @@ module "litellm_proxy" {
     "DISABLE_ADMIN_UI" = "False"
   }, var.litellm_proxy_env_vars)
   
-  secrets = merge(
-    var.litellm_proxy_secrets
-  )
+  secrets = var.litellm_proxy_secrets
+}
+
+
+module "dev" {
+  source = "../../../modules/ssca-cluster"
+
+  resource_group = "ScSc-CIO-ECT_SSCA_Cluster_Dev_RG"
+  create_resource_group = true
+  create_container_app_env = true
+  location = "canadacentral"
+  acr = {
+    name = "ectacr"
+    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
+    image = {
+      repo_name = "ssca-mcp-server"
+      tag = "1.0.1"
+    }
+  }
+  log_analytics = {
+    name = "ScSc-CIO-ECT-Infra-analytics"
+    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
+  }
+  container_app_environment_name = "ssca-cae"
+  container_app = {
+    name = "ssca-mcp-server"
+    revision_mode = "Single"
+    min_replicas  = 1
+  }
+  subscription_id = "f5fb90f1-6d1e-4a21-8935-6968d811afd8"
+  app_registration_name = "SSC-Assistant-Dev"
+
+  env_vars = var.ssca_mcp_env_vars
+  secrets  = merge({
+    "ORCHESTRATOR_LITELLM_PROXY_URL" = "https://${module.litellm_proxy.fqdn}/v1"
+  }, var.ssca_mcp_secrets)
+}
+
+
+module "geds" {
+  source = "../../../modules/ssca-cluster"
+
+  resource_group = "ScSc-CIO-ECT_SSCA_Cluster_Dev_RG"
+  create_resource_group = false
+  create_container_app_env = false
+  location = "canadacentral"
+  acr = {
+    name = "ectacr"
+    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
+    image = {
+      repo_name = "geds-mcp"
+      tag = "1.0.0"
+    }
+  }
+  log_analytics = {
+    name = "ScSc-CIO-ECT-Infra-analytics"
+    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
+  }
+  container_app_environment_name = "ssca-cae"
+  container_app = {
+    name = "geds-mcp"
+    revision_mode = "Single"
+    min_replicas = 1
+  }
+  subscription_id = "f5fb90f1-6d1e-4a21-8935-6968d811afd8"
+  app_registration_name = "SSC-Assistant-Dev"
+
+  env_vars = var.geds_mcp_env_vars
+  secrets  = var.geds_mcp_secrets
+}
+
+
+module "pmcoe" {
+  source = "../../../modules/ssca-cluster"
+
+  resource_group = "ScSc-CIO-ECT_SSCA_Cluster_Dev_RG"
+  create_resource_group = false
+  create_container_app_env = false
+  location = "canadacentral"
+  acr = {
+    name = "ectacr"
+    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
+    image = {
+      repo_name = "azure-search-mcp"
+      tag = "1.0.0"
+    }
+  }
+  log_analytics = {
+    name = "ScSc-CIO-ECT-Infra-analytics"
+    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
+  }
+  container_app_environment_name = "ssca-cae"
+  container_app = {
+    name = "pmcoe-mcp"
+    revision_mode = "Single"
+    min_replicas  = 1
+  }
+  subscription_id = "f5fb90f1-6d1e-4a21-8935-6968d811afd8"
+  app_registration_name = "SSC-Assistant-Dev"
+
+  env_vars = var.pmcoe_mcp_env_vars
+  secrets  = var.pmcoe_mcp_secrets
+}
+
+
+module "myssc" {
+  source = "../../../modules/ssca-cluster"
+
+  resource_group = "ScSc-CIO-ECT_SSCA_Cluster_Dev_RG"
+  create_resource_group = false
+  create_container_app_env = false
+  location = "canadacentral"
+  acr = {
+    name = "ectacr"
+    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
+    image = {
+      repo_name = "azure-search-mcp"
+      tag = "1.0.0"
+    }
+  }
+  log_analytics = {
+    name = "ScSc-CIO-ECT-Infra-analytics"
+    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
+  }
+  container_app_environment_name = "ssca-cae"
+  container_app = {
+    name = "myssc-mcp"
+    revision_mode = "Single"
+    min_replicas = 1
+  }
+  subscription_id = "f5fb90f1-6d1e-4a21-8935-6968d811afd8"
+  app_registration_name = "SSC-Assistant-Dev"
+
+  env_vars = var.myssc_mcp_env_vars
+  secrets  = var.myssc_mcp_secrets
+}
+
+
+module "bits" {
+  source = "../../../modules/ssca-cluster"
+
+  resource_group = "ScSc-CIO-ECT_SSCA_Cluster_Dev_RG"
+  create_resource_group = false
+  create_container_app_env = false
+  location = "canadacentral"
+  acr = {
+    name = "ectacr"
+    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
+    image = {
+      repo_name = "bits-mcp"
+      tag = "1.0.0"
+    }
+  }
+  log_analytics = {
+    name = "ScSc-CIO-ECT-Infra-analytics"
+    resource_group_name = "ScSc-CIO_ECT_Infrastructure-rg"
+  }
+  container_app_environment_name = "ssca-cae"
+  container_app = {
+    name          = "bits-mcp"
+    revision_mode = "Single"
+    min_replicas  = 1
+  }
+  subscription_id = "f5fb90f1-6d1e-4a21-8935-6968d811afd8"
+  app_registration_name = "SSC-Assistant-Dev"
+
+  env_vars = var.bits_mcp_env_vars
+  secrets  = var.bits_mcp_secrets
 }
