5183fa5312f2b0b52044494295011da40fc9f351
jeneerthan <jeneerthan.pageerathan@ssc-spc.gc.ca>
2026-05-12 10:57:25 -0400

    Fixed exposed admin_password

--- modules/postgresql-flexible/outputs.tf
+++ modules/postgresql-flexible/outputs.tf
@@ -1,4 +1,7 @@
 output "fqdn" { value = azurerm_postgresql_flexible_server.server.fqdn }
 output "admin_user" { value = azurerm_postgresql_flexible_server.server.administrator_login }
-output "admin_password" { value = azurerm_postgresql_flexible_server.server.administrator_password }
+output "admin_password" {
+  value     = azurerm_postgresql_flexible_server.server.administrator_password
+  sensitive = true
+}
 output "database_name" { value = azurerm_postgresql_flexible_server_database.db.name }
